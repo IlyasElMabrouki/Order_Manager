9742c4415eece2bff191ff98fb6cb68afe3af62e
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-03-20 17:17:53 +0000

    first commit

---
+++ customer-service/src/main/java/org/sid/customerservice/web/CustomerConfigTestController.java
@@ -0,0 +1,41 @@
+package org.sid.customerservice.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RefreshScope
+public class CustomerConfigTestController {
+
+    @Autowired
+    private Environment environment;
+
+    /*@Value("${globale.params.p1}")
+    private String p1;
+
+    @Value("${globale.params.p2}")
+    private String p2;
+
+    @Value("${customer.params.x}")
+    private String x;
+
+    @Value("${customer.params.y}")
+    private String y;*/
+
+    @GetMapping("/params")
+    public Map<String,String> params(){
+        Map<String,String> params = new HashMap<>();
+        params.put("p1",environment.getProperty("globale.params.p1"));
+        params.put("p2", environment.getProperty("globale.params.p2"));
+        params.put("x", environment.getProperty("customer.params.x"));
+        params.put("y",environment.getProperty("customer.params.y"));
+        return params;
+    }
+}
