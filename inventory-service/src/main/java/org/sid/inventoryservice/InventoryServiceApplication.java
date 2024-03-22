009e7d511e072e226357dbe84ee5617062d9d60b
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-03-22 00:15:56 +0000

    Implemented Inventory Service

--- inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,30 @@
 package org.sid.inventoryservice;
 
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository) {
+		return args -> {
+			productRepository.saveAll(List.of(
+					Product.builder().name("Computer Desktop").price(1000).quantity(10).build(),
+					Product.builder().name("Printer").price(200).quantity(5).build(),
+					Product.builder().name("Smartphone").price(500).quantity(20).build()
+			));
+		};
 	}
 
 }
